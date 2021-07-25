7ab8f8823b33c24f0453fdf9c3bc769d8bf087f3
lamisghanayem <ghanayemlamis@gmail.com>
2021-07-26 00:10:50 +0300

    demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 
 class App extends React.Component{
 
-  render(){
-    return()
+  render() {
+    return (
+      <div >
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
   }
+}
+
+export default App;
